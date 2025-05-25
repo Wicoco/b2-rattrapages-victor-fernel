ad692d92c02034ff3c130bfc80f26a7022bbed49
victorfernel <victorfernel444@gmail.com>
2025-05-26 01:44:45 +0200

    upload visual

--- 07 – Architecture orienté objet/app.js
+++ 07 – Architecture orienté objet/app.js
@@ -8,10 +8,7 @@ const app = express();
 
 // MongoDB Atlas
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connexion MongoDB OK"))
   .catch((err) => console.log(err));
 
--- 07 – Architecture orienté objet/routes/orderRoutes.js
+++ 07 – Architecture orienté objet/routes/orderRoutes.js
@@ -1,36 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Order = require('../models/Order');
+const Order = require("../models/Order");
 
 // Formulaire de commande
-router.get('/new', (req, res) => {
-  res.render('orderForm');
+router.get("/new", (req, res) => {
+  res.render("orderForm");
+});
+
+// Effacer une commande individuelle
+router.post("/delete/:id", async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.redirect("/orders/list");
+  } catch (err) {
+    res.status(500).send("Erreur lors de la suppression");
+  }
 });
 
 // Enregistrement de commande
-router.post('/new', async (req, res) => {
+router.post("/new", async (req, res) => {
   const { nom, prenom, adresse, email } = req.body;
   const prix = Math.floor(Math.random() * 100) + 1; // Prix aléatoire entre 1 et 100€
 
   const order = new Order({
-    nom, prenom, adresse, email, prix, statut: 'Commande prise en compte'
+    nom,
+    prenom,
+    adresse,
+    email,
+    prix,
+    statut: "Commande prise en compte",
   });
 
   await order.save();
-  res.redirect('/orders/list');
+  res.redirect("/orders/list");
 });
 
 // Liste des commandes
-router.get('/list', async (req, res) => {
+router.get("/list", async (req, res) => {
   const orders = await Order.find();
-  res.render('orderList', { orders });
+  res.render("orderList", { orders });
 });
 
 // MAJ du statut
-router.post('/update/:id', async (req, res) => {
+router.post("/update/:id", async (req, res) => {
   const { statut } = req.body;
   await Order.findByIdAndUpdate(req.params.id, { statut });
-  res.redirect('/orders/list');
+  res.redirect("/orders/list");
 });
 
 module.exports = router;
